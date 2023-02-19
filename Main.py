ce073fb5d3f7edc691979f84603bfbe6c90d6983
pushk <pushkarev.alexey.a@gmail.com>
2023-02-20 01:53:21 +0300

    start

---
+++ Main.py
@@ -0,0 +1,8 @@
+class product:
+    price_discount = 0.85
+
+    def __int__(self,item_name,item_price,item_amount):
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_amount = item_amount
+
