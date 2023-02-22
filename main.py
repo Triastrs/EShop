016222295d6a37abcec0c4a14aa0169a6cdef06a
pushk <pushkarev.alexey.a@gmail.com>
2023-02-22 19:02:34 +0300

    доработка класса

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-class Item:  # добовляем новый класс
+class Item:
     pay_rate = 0.85
+    examples_data = []
+    examples = []
 
     def __init__(self, item_name, item_price, amount):
         self.item_name = item_name
         self.item_price = item_price
         self.amount = amount
+        self.examples_data.append([item_name, item_price, amount])
 
     def calculate_total_price(self):
+        self.examples.append(self.item_price * self.amount)
         return self.item_price * self.amount
 
     def apply_discount(self):
+        self.examples.append(self.item_price * self.pay_rate)
         return self.item_price * self.pay_rate
 
 
@@ -23,3 +28,6 @@ Item.pay_rate = 0.8
 
 print(item1.apply_discount())
 print(item2.item_price)
+print(item1.examples_data)
+print(item1.examples)
+
