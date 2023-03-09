7f209f110ba0088d36f97c21a3943aa7a21fc3a0
pushk <pushkarev.alexey.a@gmail.com>
2023-03-09 14:20:39 +0300

    NEW dz

--- main.py
+++ main.py
@@ -1,33 +1,72 @@
+import csv
+
+
 class Item:
     pay_rate = 0.85
     examples_data = []
-    examples = []
+    initiated_examples = []
 
-    def __init__(self, item_name, item_price, amount):
-        self.item_name = item_name
+    def __init__(self, item_name: str, item_price: int, amount: int):
+        self.__item_name = item_name
         self.item_price = item_price
         self.amount = amount
-        self.examples_data.append([item_name, item_price, amount])
+        self.is_integers
+        if len(item_name) > 10:
+            raise Exception('Длина наименования товара превышает 10 символов.')
+
+    @property
+    def item_name(self) -> str:
+        """проверка, что при задании названия товара длина его не превышает 10 символов.
+        При привышении - исключение"""
+        return self.__item_name
+
+    @item_name.setter
+    def item_name(self, value: str) -> None:
+        if len(value) <= 10:
+            self.__item_name = value
+        else:
+            raise Exception('Длина наименования товара превышает 10 символов.')
+
+    @classmethod
+    def instantiate_from_csv(cls) -> 'Item':
+        """метод класса, выполняющий альтернативный способ создания объектов-товаров. Из csv-файла"""
+        with open('items.csv') as csvitems:
+            csvreader = csv.DictReader(csvitems)
+            for i in csvreader:
+                static_func = cls.is_integers(i['price'])
+                if static_func:
+                    item_name, item_price, quantity = (i['name'], float(i['price']), i['quantity'])
+                    cls.examples_data.append([item_name, int(item_price), quantity])
+                    cls.initiated_examples.append(cls(item_name, int(item_price), quantity))
+                else:
+                    item_name, item_price, quantity = (i['name'], float(i['price']), i['quantity'])
+                    cls.examples_data.append([item_name, item_price, quantity])
+                    cls.initiated_examples.append(cls(item_name, item_price, quantity))
+            return cls.initiated_examples
+
+    @staticmethod
+    def is_integers(item_price_type) -> bool:
+        """Статический метод, который проверяет, является ли число, полученое из csv-файла целым"""
+        if float(item_price_type) % 1 == 0 or float(item_price_type) % 1 == 0.0:
+            return True
+        else:
+            return False
 
     def calculate_total_price(self):
-        self.examples.append(self.item_price * self.amount)
-        return self.item_price * self.amount
+        return int(self.item_price) * int(self.amount)
 
     def apply_discount(self):
-        self.examples.append(self.item_price * self.pay_rate)
-        return self.item_price * self.pay_rate
+        return float(self.item_price) * self.pay_rate
+
+    def __repr__(self) -> str:
+        """Вохвращает содержание класса"""
+        return f"{self.__class__.__name__}('{self.__item_name}', '{self.item_price}', '{self.amount}')"
+
+    def __str__(self) -> str:
+        """Возвращает название товара"""
+        return f'{self.__item_name}'
 
 
 item1 = Item("Смартфон", 10000, 20)
-item2 = Item("Ноутбук", 20000, 5)
-
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
-
-Item.pay_rate = 0.8
-
-print(item1.apply_discount())
-print(item2.item_price)
-print(item1.examples_data)
-print(item1.examples)
-
+# item1
+print(item1)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,26 @@
 from main import Item
 
+
 example = Item("Смартфон", 10000, 20)
 
+def test_item_name():
+    assert example.item_name == 'Смартфон'
+
+def test_instantiate_from_csv():
+    assert Item.instantiate_from_csv() == Item.initiated_examples
+
+def test_is_integers():
+    assert example.is_integers(5.5) == False
+    assert example.is_integers(5) == True
 
 def test_calculate_total_price():
     assert example.calculate_total_price() == 200000
 
-
 def test_apply_discount():
-    assert example.apply_discount() == 8000.0
+    assert example.apply_discount() == 8500.0
+
+def test_repr():
+    assert example.__repr__() == "Item('Смартфон', '10000', '20')"
+
+def test_str():
+    assert example.__str__() == 'Смартфон'
